f6af7d7891ccca06483e89d64aef1db38f0b4001
remote_yz <152132476@qq.com>
2024-08-27 01:52:24 +0800

    add boost strong componet test

--- test/large_agent_test/test_large_agent_mapf_decomposition.cpp
+++ test/large_agent_test/test_large_agent_mapf_decomposition.cpp
@@ -8,6 +8,13 @@
 
 #include "common_interfaces.h"
 
+#include <boost/graph/subgraph.hpp>
+#include <boost/graph/adjacency_list.hpp>
+
+#include <boost/config.hpp>
+#include <vector>
+#include <boost/graph/connected_components.hpp>
+
 
 using namespace freeNav::LayeredMAPF::LA_MAPF;
 
@@ -15,4 +22,69 @@ using namespace freeNav::LayeredMAPF::LA_MAPF;
 TEST(basic_test, LA_MAPF_decomposition) {
     const std::string file_path = map_test_config.at("crc_ins_path");
     loadInstanceAndDecomposition<CircleAgent<2> >(file_path);
+}
+
+TEST(boost_strong_componnet, test) {
+
+    using namespace std;
+    using namespace boost;
+
+// Underlying graph representation and implementation
+//typedef adjacency_list_traits<vecS, vecS, directedS> Traits;
+
+// Graph representation
+    typedef subgraph< adjacency_list<vecS, vecS, directedS,
+            property<vertex_color_t, int>, property<edge_index_t, int> > > Graph;
+
+// Iterating over vertices and edges
+    typedef graph_traits<Graph>::vertex_iterator vertex_iter;
+//typedef graph_traits<Graph>::edge_iterator edge_iter;
+
+    Graph g;
+    add_edge(0,1, g);
+    add_edge(1,4, g);
+    add_edge(4,0, g);
+    add_edge(3,0, g);
+
+    add_edge(2,5, g);
+
+    std::vector<int> comp(num_vertices(g));
+    int num = connected_components (g, comp.data());
+    std::cout << std::endl;
+    std::vector < int >::iterator i;
+    std::cout << "Total number of components: " << num << std::endl;
+    for (i = comp.begin(); i != comp.end(); ++i)
+        std::cout << "Vertex " << i - comp.begin()
+                  << " is in component " << *i << std::endl;
+
+    std::vector<Graph* > comps(num);
+    for(size_t i=0;i<num;++i) {
+        comps[i] = & g.create_subgraph();
+    }
+    for(size_t i=0;i<num_vertices(g);++i) {
+        cout<<"add vetex "<<i<<"to sub graph "<<comp[i]<<endl;
+        add_vertex(i, *comps[comp[i]]);
+    }
+    pair<vertex_iter, vertex_iter> vip;
+
+    cout << "Vertices in g  = [ ";
+    vip = vertices(g);
+    for(vertex_iter vi = vip.first; vi != vip.second; ++vi) {
+        cout << *vi << " ";
+    }
+    cout << "]" << endl;
+    for(size_t i=0;i<num;i++)
+    {
+        cout << "Vertices (local) in comps[i]' = [ ";
+        pair<vertex_iter, vertex_iter> lvip;
+        lvip = vertices(*comps[i]);
+
+        for(vertex_iter vi = lvip.first; vi != lvip.second; ++vi)
+        {
+            cout << (*comps[i]).local_to_global(*vi) << " ";
+        }
+
+        cout << "]" << endl;
+    }
+
 }
