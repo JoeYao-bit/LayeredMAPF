cbd0ff3a3e44e8bc77968c2445ad033ab438860e
yaozhuo <1521232476@qq.com>
2025-06-22 15:48:06 +0800

    update

--- algorithm/LA-MAPF/script/MAPF_decomposition_analysis.py
+++ algorithm/LA-MAPF/script/MAPF_decomposition_analysis.py
@@ -2,6 +2,7 @@ import matplotlib as mp
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import ticker
+import os
 
 #import seaborn as sns
 
@@ -26,11 +27,10 @@ def loadDataFromfile(file_path):
                 new_data.memory_usage      = float(splited_line[5])
                 new_data.num_of_subproblem = float(splited_line[6])
                 
-                new_data.init_time_cost    = float(splited_line[7])
-                new_data.level_1_time_cost = float(splited_line[8])
-                new_data.level_2_time_cost = float(splited_line[9])
-                new_data.level_3_time_cost = float(splited_line[10])
-                new_data.level_4_time_cost = float(splited_line[11])
+                new_data.level_1           = float(splited_line[7])
+                new_data.level_2           = float(splited_line[8])
+                new_data.level_3           = float(splited_line[9])
+                #new_data.level_4           = float(splited_line[10])
 
                 data_list.append(new_data)
     except Exception as e:            
@@ -45,12 +45,11 @@ class LineData:
     level             = 0
     memory_usage      = 0.
     num_of_subproblem = 0
-    
-    init_time_cost    = 0.
-    level_1_time_cost = 0.
-    level_2_time_cost = 0.
-    level_3_time_cost = 0.
-    level_4_time_cost = 0.    
+    level_1           = 0.
+    level_2           = 0.
+    level_3           = 0.
+    level_4           = 0.
+        
 
 # all data in a txt file
 class SingleTestData:
@@ -59,11 +58,11 @@ class SingleTestData:
     
     
 def drawMethodMap(single_map_data, value_type):
-    fig = plt.figure(figsize=(5,3.5)) 
+    fig = plt.figure(figsize=(5,3.5)) #添加绘图框
     map_name = single_map_data.map_name
     
-    all_raw_data = [dict(), dict(), dict(), dict()]
-    all_init_time_cost = dict() # only use when draw time cost of initialize cconnectivity graph
+    all_raw_data = dict()
+
 
     for data in single_map_data.data_list:
         total_size        = data.total_size
@@ -72,65 +71,39 @@ def drawMethodMap(single_map_data, value_type):
         max_cluster       = data.max_cluster
         memory_usage      = data.memory_usage
         num_of_subproblem = data.num_of_subproblem
+        level             = data.level
         
-        if all_raw_data[0].get(total_size) == None:
-            all_raw_data[0][total_size] = list()
-            all_raw_data[1][total_size] = list()
-            all_raw_data[2][total_size] = list()            
-            all_raw_data[3][total_size] = list()            
+        if all_raw_data.get(level) == None:
+            all_raw_data[level] = dict()
 
-        assert(data.level>= 1 and data.level <= 4)
+        if all_raw_data[level].get(total_size) == None:
+            all_raw_data[level][total_size] = list()        
         
         if value_type == "decomposition_rate":
-            all_raw_data[data.level-1][total_size].append(max_cluster / total_size)    
+            all_raw_data[level][total_size].append(max_cluster / total_size)    
         elif value_type == "time_cost":
-            all_raw_data[data.level-1][total_size].append(time_cost)    
-            if all_init_time_cost.get(total_size)  == None:
-                all_init_time_cost[total_size] = list()   
-            all_init_time_cost[total_size].append(data.init_time_cost)
+            all_raw_data[level][total_size].append(time_cost)    
         elif value_type == "memory_usage":
-            all_raw_data[data.level-1][total_size].append(memory_usage)        
+            all_raw_data[level][total_size].append(memory_usage)        
         elif value_type == "num_of_subproblem":
-            all_raw_data[data.level-1][total_size].append(num_of_subproblem)     
-    label_buffer = "1"        
-    for i in range(0,4):    
-        
-        dict_x_y = dict()
-        dict_x_error = dict()
+            all_raw_data[level][total_size].append(num_of_subproblem)  
 
-        if value_type == "decomposition_rate":
-            for data_key, data_val in all_raw_data[i].items():
-                dict_x_y[data_key] = np.mean(data_val)
-                dict_x_error[data_key] = np.std(data_val)
-        else:
-            for data_key, data_val in all_raw_data[i].items():
-                dict_x_y[data_key] = np.mean(data_val)
-                dict_x_error[data_key] = np.std(data_val)
+
+    for level_key, level_value in all_raw_data.items():    
+        x = list()
+        y = list()    
+        std_val = list()
+
+        for data_key, data_val in all_raw_data[level_key].items():
+            x.append(data_key)        
+            y.append(np.mean(data_val))
+            std_val.append(np.std(data_val))
+
 
         # yerr=std_val
-        sorted_x = sorted (dict_x_y)
-        sorted_y = list()
-        sorted_error = list()
-        for temp_x in sorted_x:
-            sorted_y.append(dict_x_y[temp_x])
-            sorted_error.append(dict_x_error[temp_x])
-        plt.errorbar(sorted_x, sorted_y, label=label_buffer, markersize=14, fmt=step_fmt[i], linewidth= 4, elinewidth=4, capsize=4)
-        label_buffer = label_buffer + ", " + str(i+2)          
+        plt.errorbar(x, y, label=level_flag_map[level_key], markersize=14, fmt=step_fmt[level_key], linewidth= 4, elinewidth=4, capsize=4)
+        #break    
 
-    # add time cost about construct connectivity graph
-    if value_type == "time_cost":
-        #print(all_init_time_cost)
-        for data_key, data_val in all_init_time_cost.items():
-            dict_x_y[data_key] = np.mean(data_val)
-            dict_x_error[data_key] = np.std(data_val)
-        sorted_x = sorted (dict_x_y)
-        sorted_y = list()
-        sorted_error = list()
-        for temp_x in sorted_x:
-            sorted_y.append(dict_x_y[temp_x])
-            sorted_error.append(dict_x_error[temp_x])
-        plt.errorbar(sorted_x, sorted_y, label="init_cost", markersize=14, fmt=step_fmt[4], linewidth= 4, elinewidth=4, capsize=4)
-        
     plt.legend(loc='best')    
     plt.tick_params(axis='both', labelsize=18)
     #plt.ticklabel_format(style='sci', scilimits=(0,0), axis='y')
@@ -138,10 +111,8 @@ def drawMethodMap(single_map_data, value_type):
     formater = ticker.ScalarFormatter(useMathText=True) 
     formater.set_scientific(True)
     formater.set_powerlimits((0,0))
-
+    
     ax = plt.gca()
-    if value_type == "time_cost":
-        ax.set_yscale('log')  
     ax.yaxis.offsetText.set_fontsize(18)
     ax.yaxis.set_major_formatter(formater)
     y_range = plt.ylim()
@@ -150,22 +121,23 @@ def drawMethodMap(single_map_data, value_type):
         plt.ylim(0, 1)
 
     plt.legend(loc='best', fontsize = 16, ncol=1, handletextpad=.5, framealpha=0.5)
-    # plt.title(title_dict[value_type])
-
     #plt.grid()
     plt.tight_layout()
-    save_path = '../../../test/test_data/large_agent_instance/decomposition/'+value_type+'/'+map_name+"-"+value_type+'.png'
+    save_path = data_path_dir+'decomposition/'+value_type+'/'
+
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+        print("Folder: " + save_path + " created")
+
+    save_path = save_path +map_name+"-"+value_type+'.png'
     plt.savefig(save_path, dpi = 200, bbox_inches='tight')   
     plt.close()
     print("save picture to "+save_path)
 
     
-step_fmt = ["x-","X-.","*--","2--","r-"] # ["x-","o-.","^--","s--"]       
-title_dict = {"time_cost":"Time cost(ms)",
-              "decomposition_rate":"Decomposition rate",
-              "memory_usage":"Memory usage(MB)",
-              "num_of_subproblem":"Number of subproblem"
-              }
+step_fmt = {3:"x-", 4:"o-.", 5:"^--"}    
+
+level_flag_map = {3:"Raw-Bi", 4:"Bi-level", 5:"Break loop"}
     
 data_path_dir = '../../../test/test_data/large_agent_instance/'
 all_map_name = [
@@ -209,8 +181,14 @@ all_map_name = [
                 "AR0072SR",
                 "Denver_2_256"
                 ]
-
 all_single_data = list()
+
+
+title_dict = {"time_cost":"Time cost(ms)",
+              "decomposition_rate":"Decomposition rate",
+              "memory_usage":"Memory usage(MB)",
+              "num_of_subproblem":"Number of subproblem"
+              }
 
 for map_name in all_map_name:
     data_file_path = data_path_dir + map_name + '_la_dec.txt'
@@ -218,9 +196,11 @@ for map_name in all_map_name:
     single = SingleTestData()
     single.map_name = map_name
     single.data_list = loadDataFromfile(data_file_path)
+    #break
     if len(single.data_list) == 0:
         continue
     all_single_data.append(single)
+    #break
     
 for single_map_data in all_single_data:
     drawMethodMap(single_map_data, "time_cost")
