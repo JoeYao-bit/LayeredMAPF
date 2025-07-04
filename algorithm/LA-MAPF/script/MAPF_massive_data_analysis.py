c5b370395b7b464f7d14ee38a71920e266984d30
yaozhuo <1521232476@qq.com>
2025-07-05 04:24:04 +0800

    update

--- algorithm/LA-MAPF/script/MAPF_massive_data_analysis.py
+++ algorithm/LA-MAPF/script/MAPF_massive_data_analysis.py
@@ -765,4 +765,6 @@ def removeMethodDataFromFiles(map_format_map_index_local, method_name_local):
         print('remove data of ', method_name_local, ' from', data_file_path)
         removeMethodDataFromFile(data_file_path, method_name_local)
     
-#removeMethodDataFromFiles(map_format_map, 'ID_CBS')
+# removeMethodDataFromFiles(map_format_map, 'RAW_LaCAM')
+# removeMethodDataFromFiles(map_format_map, 'BP_LaCAM')
+# removeMethodDataFromFiles(map_format_map, 'BL_LaCAM')
