40e1442a6de8450606480bfa51791b433ec755a3
yaozhuo <1521232476@qq.com>
2026-01-04 02:17:56 +0800

    update

--- test/large_agent_test/test_massive_laryered_lamapf.cpp
+++ test/large_agent_test/test_massive_laryered_lamapf.cpp
@@ -6,6 +6,7 @@
 
 #include "../../algorithm/LA-MAPF/LaCAM/layered_large_agent_LaCAM.h"
 #include "../../algorithm/LA-MAPF/CBS/layered_large_agent_CBS.h"
+#include "../../algorithm/LA-MAPF/CBSH2-RTC/inc/driver.h"
 
 #include "common_interfaces.h"
 
